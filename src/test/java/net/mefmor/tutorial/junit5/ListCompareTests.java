95c0d774da43b5a904abd7dc1f834eb65b45dc75
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-04-17 14:48:04 +0300

    Add tests to ListCompareTests

--- src/test/java/net/mefmor/tutorial/junit5/ListCompareTests.java
+++ src/test/java/net/mefmor/tutorial/junit5/ListCompareTests.java
@@ -9,9 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ListCompareTests {
     @Test
-    void compareContent() {
+    void testContainsExactly() {
         List<String> list = Arrays.asList("First", "Second");
 
         assertThat(list).containsExactly("First", "Second");
+        assertThat(list).containsOnly("First", "Second");
+        assertThat(list).containsExactlyInAnyOrder("Second", "First");
+        assertThat(list).containsAnyOf("Second", "Third");
     }
 }

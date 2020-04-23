15cd4b5527332c5334f5d38c4835a4c04ec45409
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-04-23 09:15:23 +0300

    Add web tests

---
+++ src/main/java/net/mefmor/tutorial/spring/boot/testing/HomeController.java
@@ -0,0 +1,12 @@
+package net.mefmor.tutorial.spring.boot.testing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/net/mefmor/tutorial/spring/boot/testing/web/HomePageBrowserTest.java
@@ -0,0 +1,45 @@
+package net.mefmor.tutorial.spring.boot.testing.web;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HomePageBrowserTest {
+    private static HtmlUnitDriver browser;
+
+    @BeforeAll
+    static void setup() {
+        browser = new HtmlUnitDriver();
+
+        browser.manage().timeouts()
+                .implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    static void teardown() {
+        browser.quit();
+    }
+
+    @Test
+    void testHomePage(@LocalServerPort int port) {
+        String homePage = "http://localhost:" + port;
+        System.out.println(homePage);
+        browser.get(homePage);
+
+        assertThat(browser.getTitle()).isEqualTo("Spring Boot Tutorial");
+        assertThat(browser.findElementByTagName("h1").getText()).isEqualTo("Welcome to...");
+
+        String imgSrc = browser.findElementByTagName("img").getAttribute("src");
+        assertThat(imgSrc).isEqualTo(homePage + "/images/TacoCloud.png");
+    }
+
+}

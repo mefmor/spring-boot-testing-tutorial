b4fc5d707386b6f0d75d0ab930be3088cafa894b
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-04-24 07:35:51 +0300

    Add lombok and remove unusable methods

--- src/test/java/net/mefmor/tutorial/spring/boot/testing/database/Customer.java
+++ src/test/java/net/mefmor/tutorial/spring/boot/testing/database/Customer.java
@@ -1,12 +1,15 @@
 package net.mefmor.tutorial.spring.boot.testing.database;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Customer {
+@Data
+class Customer {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -14,29 +17,8 @@ public class Customer {
     private String firstName;
     private String lastName;
 
-    protected Customer() {}
-
-    public Customer(String firstName, String lastName) {
+    Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Customer[id=%d, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
     }
 }
